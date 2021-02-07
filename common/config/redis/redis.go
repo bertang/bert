24490794ac0560c98a7ad37364b8c6c15f79815b
tangbo <tangbo@kaikeba.com>
2021-02-07 18:22:12 +0800

    ok

--- common/config/redis/redis.go
+++ common/config/redis/redis.go
@@ -1,47 +1,49 @@
 //@Title redis.go
-//@Description 
+//@Description
 //@Author bertang
 //@Created bertang 2021/2/7 11:53 上午
 package redis
 
 import (
-    "fmt"
-    "github.com/bertang/bert/common/config"
-    "sync"
+	"fmt"
+	"sync"
+
+	"github.com/bertang/bert/common/config"
 )
 
 var (
-    redisConf  *RedisConf
-    once       sync.Once
-    dbFilePath = &config.FilePath{Filename: "redis.yml"}
+	redisConf  *RedisConf
+	once       sync.Once
+	dbFilePath = &config.FilePath{Filename: "redis.yml"}
 )
 
 //initRedis 初始化redis
 func initRedis() {
-    once.Do(func() {
-        redisConf = &RedisConf{}
-        config.Register(dbFilePath, redisConf)
-    })
+	once.Do(func() {
+		redisConf = &RedisConf{}
+		config.Register(dbFilePath, redisConf)
+	})
 
 }
 
 //RedisConf redis配置
 type RedisConf struct {
-    Host      string
-    Port      int
-    Auth      string
-    MaxIdle   int `mapstructure:"max_idle"`
-    MaxActive int `mapstructure:"max_active"`
+	Host      string
+	Port      int
+	Auth      string
+	MaxIdle   int `mapstructure:"max_idle"`
+	MaxActive int `mapstructure:"max_active"`
 }
 
 //String 返回
 func (r *RedisConf) String() string {
-    return fmt.Sprintf("%s:%d", redisConf.Host, redisConf.Port)
+	return fmt.Sprintf("%s:%d", redisConf.Host, redisConf.Port)
 }
 
+//GetRedisConf 获取redis配置对象
 func GetRedisConf() *RedisConf {
-    if redisConf == nil {
-        initRedis()
-    }
-    return redisConf
+	if redisConf == nil {
+		initRedis()
+	}
+	return redisConf
 }
